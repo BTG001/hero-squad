62f07fd27abafabead722c50e600f35607fac8dc
BTG001 <brunokipkorir2@gmail.com>
2020-07-06 02:53:52 +0300

    Create test file for Hero.java class and @Before and @After annotations will help us make our testing process easier and more concise

---
+++ src/test/java/HeroTest.java
@@ -0,0 +1,16 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
