d98a54aad82b5408d11e788f020178fde43aaccf
BTG001 <brunokipkorir2@gmail.com>
2020-07-06 03:21:41 +0300

    Test to make sure we can assign each Hero some content and then retrieve it

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,14 +1,19 @@
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class HeroTest {
     @Test
     public void checkIfItInstantiatesProperly() {
         Hero hero = new Hero("Shazam", 30, "Superhuman Strength", "Spell Casting Confusion");
         assertEquals(true, hero instanceof Hero);
+    }
+
+    @Test
+    public void getsNameInput() {
+        Hero hero = new Hero("Shazam", 30, "Superhuman Strength", "Spell Casting Confusion");
+        assertEquals("Shazam", hero.getName());
     }
 }
