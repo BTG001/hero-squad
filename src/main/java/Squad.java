cf72d9d52f9307f5f87cf114878ca9e9084bed45
BTG001 <brunokipkorir2@gmail.com>
2020-07-06 13:03:29 +0300

    Declare the get method to get the squadName to make trhe test pass

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -2,12 +2,18 @@ import java.util.ArrayList;
 
 public class Squad {
     private final int size;
+    private final String squadName;
 
     public Squad(int size, String squadName, String cause) {
         this.size = size;
+        this.squadName = squadName;
     }
 
     public int getSize() {
         return size;
+    }
+
+    public String getSquadName() {
+        return squadName;
     }
 }
