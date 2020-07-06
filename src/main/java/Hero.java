a491830d9be4b052cf9822723bce8dcd6b57b230
BTG001 <brunokipkorir2@gmail.com>
2020-07-06 03:25:53 +0300

    Declare get method for name and test passes

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,6 +1,13 @@
 import java.util.ArrayList;
 
 public class Hero {
+    private final String name;
+
     public Hero(String name, int age, String specialPower, String weakness) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
