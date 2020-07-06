433e3285fb2082c853b04db7f30f87122f2bf0bf
BTG001 <brunokipkorir2@gmail.com>
2020-07-06 03:31:34 +0300

    Declare get method for age and the test passes

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -2,12 +2,18 @@ import java.util.ArrayList;
 
 public class Hero {
     private final String name;
+    private final int age;
 
     public Hero(String name, int age, String specialPower, String weakness) {
         this.name = name;
+        this.age = age;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
