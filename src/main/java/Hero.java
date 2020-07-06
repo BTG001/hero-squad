d75fb562c14a833b2c67874fc1870852e5bdde0c
BTG001 <brunokipkorir2@gmail.com>
2020-07-06 03:47:25 +0300

    Declare the get method for weakness and the test passes

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -4,11 +4,13 @@ public class Hero {
     private final String name;
     private final int age;
     private final String specialPower;
+    private final String weakness;
 
     public Hero(String name, int age, String specialPower, String weakness) {
         this.name = name;
         this.age = age;
         this.specialPower = specialPower;
+        this.weakness = weakness;
     }
 
     public String getName() {
@@ -21,5 +23,9 @@ public class Hero {
 
     public String getSpecialPower() {
         return specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
     }
 }
