b308ee88990c59e3c772e209a4188b0181a60b33
BTG001 <brunokipkorir2@gmail.com>
2020-07-06 16:39:48 +0300

    Make a new Squad object, call it newSquad.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,6 +21,7 @@ public class App {
             String name = request.queryParams("squadName");
             String cause = request.queryParams("cause");
             int size=Integer.parseInt(request.queryParams("size"));
+            Squad newSquad = new Squad(size,name, cause);
         });
     }
 }
