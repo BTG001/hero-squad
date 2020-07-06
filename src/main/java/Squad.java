4dc5ca7e981f14c9e2a7384d3373218405fc5c58
BTG001 <brunokipkorir2@gmail.com>
2020-07-06 13:08:13 +0300

    Declare get method to retrieve the cause and hence the test passes

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 public class Squad {
     private final int size;
     private final String squadName;
+    private final String cause;
 
     public Squad(int size, String squadName, String cause) {
         this.size = size;
         this.squadName = squadName;
+        this.cause = cause;
     }
 
     public int getSize() {
@@ -15,5 +17,9 @@ public class Squad {
 
     public String getSquadName() {
         return squadName;
+    }
+
+    public String getCause() {
+        return cause;
     }
 }
