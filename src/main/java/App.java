c2ac74cb9cc1136f98cf09bdde2b843371ff7165
BTG001 <brunokipkorir2@gmail.com>
2020-07-15 11:55:17 +0300

    Apply CRUD principle and coded RESTful routes according to REST standards

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,34 +18,6 @@ public class App {
         port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
-        get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
-        get("/squads/new", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "squadform.hbs");
-        }, new HandlebarsTemplateEngine());
-        post("/squads/new", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            String name = request.queryParams("squadName");
-            String cause = request.queryParams("cause");
-            int size=Integer.parseInt(request.queryParams("size"));
-            Squad newSquad = new Squad(size,name, cause);
-            model.put("squads", Squad.getAll());
-            return new ModelAndView(model, "success.hbs");
-        }, new HandlebarsTemplateEngine());
-        get("/receivedsquad", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            ArrayList<Squad> squads = Squad.getAll();
-            model.put("squads", squads);
-            return new ModelAndView(model, "display.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/herosform/new", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "heroform-input.hbs");
-        }, new HandlebarsTemplateEngine());
         post("/herosform/new", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             String name = request.queryParams("name");
@@ -56,6 +28,10 @@ public class App {
             model.put("heros", Hero.getAll());
             return new ModelAndView(model, "herosuccess.hbs");
         }, new HandlebarsTemplateEngine());
+        get("/herosform/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "heroform-input.hbs");
+        }, new HandlebarsTemplateEngine());
         get("/receivedheroes", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             ArrayList<Hero> heroes = Hero.getAll();
@@ -63,6 +39,33 @@ public class App {
             return new ModelAndView(model, "displayheroes.hbs");
         }, new HandlebarsTemplateEngine());
 
+
+        post("/squads/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("squadName");
+            String cause = request.queryParams("cause");
+            int size=Integer.parseInt(request.queryParams("size"));
+            Squad newSquad = new Squad(size,name, cause);
+            model.put("squads", Squad.getAll());
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/squads/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "squadform.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/receivedsquad", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Squad> squads = Squad.getAll();
+            model.put("squads", squads);
+            return new ModelAndView(model, "display.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
         get("/success", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "success.hbs");
